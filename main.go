07c45c1ac2a072bb73d3b18f30db13748a907c78
quaminigin <qiuning126@126.com>
2025-07-27 13:34:36 +0800

    Add files via upload

--- main.go
+++ main.go
@@ -1,7 +1,26 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+
+	"github.com/quaminigin/go_test/class_01"
+)
 
 func main() {
-	fmt.Println("Hello, Go Modules!")
+	// 两数之和
+	// fmt.Println(class_01.SumTwoNumbers())
+	// 合并区间
+	// fmt.Println(class_01.MergeIntervals())
+	// 删除有序数组中的重复项
+	// fmt.Println(class_01.DeleteDumplicate())
+	// 加一
+	// fmt.Println(class_01.Add1())
+	// 最长公共前缀
+	// fmt.Println(class_01.LongestCommonPrefix())
+	// 有效的括号
+	// fmt.Println(class_01.CheckBracket("[(){}][]{}({[()[{}]]})"))
+	// 回文数
+	// fmt.Println(class_01.CheckPalindrome(109088880901))
+	// 只出现一次的数字
+	fmt.Println(class_01.SearchSingle())
 }

e7b697aa179011a04514a7fd72fb745f2a96e1e3
Michael <michael@xl-dev.com>
2025-07-28 15:41:40 +0800

    先提交4题

--- class_02/answer.go
+++ class_02/answer.go
@@ -1,1 +1,72 @@
 package class_02
+
+import (
+	"fmt"
+	"sync"
+	"time"
+)
+
+/*
+指针
+1. 题目 ：编写一个Go程序，定义一个函数，该函数接收一个整数指针作为参数，
+在函数内部将该指针指向的值增加10，然后在主函数中调用该函数并输出修改后的值。
+考察点 ：指针的使用、值传递与引用传递的区别。
+*/
+func Add10(p *int) {
+	*p += 10
+}
+
+/*
+指针
+2. 题目 ：实现一个函数，接收一个整数切片的指针，将切片中的每个元素乘以2。
+考察点 ：指针运算、切片操作。
+*/
+func TimeSliceBy2(s *[]int) {
+	for index := range *s {
+		(*s)[index] *= 2
+	}
+}
+
+/*
+Goroutine
+1. 题目 ：编写一个程序，使用 go 关键字启动两个协程，一个协程打印从1到10的奇数，另一个协程打印从2到10的偶数。
+考察点 ： go 关键字的使用、协程的并发执行。
+*/
+func PrintWith2Go(wg *sync.WaitGroup) {
+	go func() {
+		for i := 1; i < 10; i += 2 {
+			fmt.Println(i)
+		}
+		defer wg.Done()
+	}()
+
+	go func() {
+		for i := 2; i < 11; i += 2 {
+			fmt.Println(i)
+		}
+		defer wg.Done()
+	}()
+
+}
+
+/*
+Goroutine
+2. 题目 ：设计一个任务调度器，接收一组任务（可以用函数表示），并使用协程并发执行这些任务，同时统计每个任务的执行时间。
+考察点 ：协程原理、并发任务调度。
+*/
+func TaskScheduling(tasks []func()) {
+	timeStatistics := make(map[int]int64)
+	var wg sync.WaitGroup
+	wg.Add(len(tasks))
+	for index, task := range tasks {
+		go func() {
+			startTime := time.Now().UnixNano()
+			task()
+			interval := time.Now().UnixNano() - startTime
+			timeStatistics[index] = interval
+			wg.Done()
+		}()
+	}
+	wg.Wait()
+	fmt.Println(timeStatistics)
+}
--- main.go
+++ main.go
@@ -2,25 +2,74 @@ package main
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/quaminigin/go_test/class_01"
+	"github.com/quaminigin/go_test/class_02"
 )
 
 func main() {
+	// 第2课习题测试
+	Class_02_Test()
+
+	// 第1课习题测试
+	// Class_01_Test()
+
+}
+
+/*
+第2课习题测试
+*/
+func Class_02_Test() {
+	// 1. 指针 - 加10
+	// var num int = 100
+	// class_02.Add10(&num)
+	// fmt.Println(num)
+
+	// 2. 指针 - 乘以2
+	// numlist := []int{1, 3, 5, 2, 8, 10, 0, 1}
+	// class_02.TimeSliceBy2(&numlist)
+	// fmt.Println(numlist)
+
+	// 3. 协程 - 打印奇偶序列
+	// var wg sync.WaitGroup
+	// wg.Add(2)
+	// class_02.PrintWith2Go(&wg)
+	// wg.Wait()
+	// fmt.Println("Over")
+
+	// 4. 协程 - 任务调度打印时间
+	class_02.TaskScheduling([]func(){
+		func() {
+			time.Sleep(1 * time.Second)
+		},
+		func() {
+			time.Sleep(4 * time.Second)
+		},
+		func() {
+			time.Sleep(10 * time.Second)
+		},
+	})
+}
+
+/*
+第1课习题测试
+*/
+func Class_01_Test() {
 	// 两数之和
-	// fmt.Println(class_01.SumTwoNumbers())
+	fmt.Println(class_01.SumTwoNumbers())
 	// 合并区间
-	// fmt.Println(class_01.MergeIntervals())
+	fmt.Println(class_01.MergeIntervals())
 	// 删除有序数组中的重复项
-	// fmt.Println(class_01.DeleteDumplicate())
+	fmt.Println(class_01.DeleteDumplicate())
 	// 加一
-	// fmt.Println(class_01.Add1())
+	fmt.Println(class_01.Add1())
 	// 最长公共前缀
-	// fmt.Println(class_01.LongestCommonPrefix())
+	fmt.Println(class_01.LongestCommonPrefix())
 	// 有效的括号
-	// fmt.Println(class_01.CheckBracket("[(){}][]{}({[()[{}]]})"))
+	fmt.Println(class_01.CheckBracket("[(){}][]{}({[()[{}]]})"))
 	// 回文数
-	// fmt.Println(class_01.CheckPalindrome(109088880901))
+	fmt.Println(class_01.CheckPalindrome(109088880901))
 	// 只出现一次的数字
 	fmt.Println(class_01.SearchSingle())
 }

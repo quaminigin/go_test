227d511b6adbe4d2cc90dd1ac6644ea60198b13b
Michael <michael@xl-dev.com>
2025-07-28 20:41:09 +0800

    第3课作业

--- class_03/answer.go
+++ class_03/answer.go
@@ -1,1 +1,246 @@
 package class_03
+
+import (
+	"errors"
+	"fmt"
+
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+)
+
+/*
+SQL语句练习
+题目1：基本CRUD操作
+假设有一个名为 students 的表，包含字段
+id （主键，自增）、
+name （学生姓名，字符串类型）、
+age （学生年龄，整数类型）、
+grade （学生年级，字符串类型）。
+要求 ：
+编写SQL语句向 students 表中插入一条新记录，学生姓名为 "张三"，年龄为 20，年级为 "三年级"。
+编写SQL语句查询 students 表中所有年龄大于 18 岁的学生信息。
+编写SQL语句将 students 表中姓名为 "张三" 的学生年级更新为 "四年级"。
+编写SQL语句删除 students 表中年龄小于 15 岁的学生记录。
+*/
+
+/*
+CREATE TABLE `students`
+(`id` bigint AUTO_INCREMENT,`name` varchar,`age` smallint,`grade` varchar,PRIMARY KEY (`id`))
+*/
+type Student struct {
+	ID    uint64 `gorm:"type:bigint;primaryKey;autoIncrement"`
+	Name  string `gorm:"type:varchar(120);size:120"`
+	Age   uint16 `gorm:"type:smallint"`
+	Grade string `gorm:"type:varchar(120);size:120"`
+}
+
+func BasicSQL(step string) {
+	db, err := gorm.Open(mysql.Open("root:root@tcp(127.0.0.1:3306)/go_test?charset=utf8mb4&parseTime=True&loc=Local"))
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&Student{})
+	switch step {
+	case "create":
+		s := &Student{Name: "张三", Age: 20, Grade: "三年级"}
+		db.Create(s)
+	case "query":
+		ss := []Student{}
+		db.Where("age > ?", 18).Find(&ss)
+		fmt.Println(ss)
+	case "update":
+		db.Model(&Student{}).Where("name = ?", "张三").Update("grade", "四年级")
+	case "delete":
+		db.Where("age < ?", 15).Delete(&Student{})
+	default:
+		panic("Step string is illegal")
+	}
+}
+
+/*
+SQL语句练习
+题目2：事务语句
+假设有两个表： accounts 表（包含字段 id 主键， balance 账户余额）和
+transactions 表（包含字段 id 主键， from_account_id 转出账户ID， to_account_id 转入账户ID， amount 转账金额）。
+要求 ：
+编写一个事务，实现从账户 A 向账户 B 转账 100 元的操作。
+在事务中，需要先检查账户 A 的余额是否足够，如果足够则从账户 A 扣除 100 元，
+向账户 B 增加 100 元，并在 transactions 表中记录该笔转账信息。如果余额不足，则回滚事务。
+*/
+type Account struct {
+	ID      uint64 `gorm:"type:bigint;primaryKey;autoIncrement"`
+	Balance int64  `gorm:"type:bigint"`
+}
+
+type Transaction struct {
+	ID            uint64 `gorm:"type:bigint;primaryKey;autoIncrement"`
+	FromAccountID uint64 `gorm:"type:bigint"`
+	ToAccountID   uint64 `gorm:"type:bigint"`
+	Amount        int64  `gorm:"type:bigint"`
+}
+
+func TransactionSQL() {
+	db, err := gorm.Open(mysql.Open("root:root@tcp(127.0.0.1:3306)/go_test?charset=utf8mb4&parseTime=True&loc=Local"))
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&Account{})
+	db.AutoMigrate(&Transaction{})
+	db.Transaction(func(tx *gorm.DB) error {
+
+		var accA Account
+		var accB Account
+		if err := tx.First(&accA, 1).Error; err != nil {
+			return err
+		} else if accA.Balance < 10000 {
+			return errors.New("账号A余额不足")
+		} else if err := tx.First(&accB, 2).Error; err != nil {
+			return err
+		} else {
+			accA.Balance -= 10000
+			accB.Balance += 10000
+			txRecord := Transaction{FromAccountID: accA.ID, ToAccountID: accB.ID, Amount: 10000}
+			if err := tx.Save(accA).Error; err != nil {
+				return err
+			} else if err := tx.Save(accB).Error; err != nil {
+				return err
+			} else if err := tx.Create(&txRecord).Error; err != nil {
+				return err
+			} else {
+				return nil
+			}
+		}
+	})
+}
+
+/*
+进阶gorm
+题目1：模型定义
+假设你要开发一个博客系统，有以下几个实体： User （用户）、 Post （文章）、 Comment （评论）。
+要求 ：
+使用Gorm定义 User 、 Post 和 Comment 模型，
+其中 User 与 Post 是一对多关系（一个用户可以发布多篇文章），
+Post 与 Comment 也是一对多关系（一篇文章可以有多个评论）。
+编写Go代码，使用Gorm创建这些模型对应的数据库表。
+*/
+type User struct {
+	gorm.Model
+	Username   string `gorm:"unique"`
+	PostsCount int
+	Posts      []Post
+}
+
+type Post struct {
+	gorm.Model
+	Title         string
+	Content       string
+	CommentsCount int
+	CommentStatus string
+	UserID        uint
+	Comments      []Comment
+}
+
+type Comment struct {
+	gorm.Model
+	Title   string
+	Content string
+	PostID  uint
+	UserID  uint
+}
+
+func BlogCreateTable() {
+	db, err := gorm.Open(mysql.Open("root:root@tcp(127.0.0.1:3306)/go_test?charset=utf8mb4&parseTime=True&loc=Local"))
+	if err != nil {
+		panic(err)
+	}
+	db.AutoMigrate(&User{})
+	db.AutoMigrate(&Post{})
+	db.AutoMigrate(&Comment{})
+}
+
+/*
+进阶gorm
+题目2：关联查询
+基于上述博客系统的模型定义。
+要求 ：
+编写Go代码，使用Gorm查询某个用户发布的所有文章及其对应的评论信息。
+编写Go代码，使用Gorm查询评论数量最多的文章信息。
+*/
+func LinkedTableQuery() (*User, *Post) {
+	db, err := gorm.Open(mysql.Open("root:root@tcp(127.0.0.1:3306)/go_test?charset=utf8mb4&parseTime=True&loc=Local"))
+	if err != nil {
+		panic(err)
+	}
+	var user User
+	db.Preload("Posts.Comments").Preload("Posts").Where("username = ?", "Lucy").First(&user)
+	if len(user.Posts) > 0 {
+		mostCommented := user.Posts[0]
+		maxNum := len(mostCommented.Comments)
+		for i := 1; i < len(user.Posts); i++ {
+			if len(user.Posts[i].Comments) > maxNum {
+				mostCommented = user.Posts[i]
+				maxNum = len(user.Posts[i].Comments)
+			}
+		}
+		return &user, &mostCommented
+	} else {
+		return &user, nil
+	}
+}
+
+/*
+进阶gorm
+题目3：钩子函数
+继续使用博客系统的模型。
+要求 ：
+为 Post 模型添加一个钩子函数，在文章创建时自动更新用户的文章数量统计字段。
+为 Comment 模型添加一个钩子函数，在评论删除时检查文章的评论数量，如果评论数量为 0，则更新文章的评论状态为 "无评论"。
+*/
+func (p *Post) AfterCreate(tx *gorm.DB) error {
+	tx.Model(&User{}).Where("id = ?", p.UserID).Update("posts_count", gorm.Expr("posts_count + 1"))
+	return nil
+}
+
+func (p *Post) AfterDelete(tx *gorm.DB) error {
+	tx.Model(&User{}).Where("id = ?", p.UserID).Update("posts_count", gorm.Expr("posts_count - 1"))
+	return nil
+}
+
+func (c *Comment) AfterCreate(tx *gorm.DB) error {
+	tx.Model(&Post{}).Where("id = ?", c.PostID).
+		Updates(map[string]interface{}{"comments_count": gorm.Expr("comments_count + 1"), "comment_status": "有评论"})
+	return nil
+}
+
+func (c *Comment) AfterDelete(tx *gorm.DB) error {
+	var post Post
+	tx.First(&post, c.PostID)
+	if post.CommentsCount < 2 {
+		tx.Model(&Post{}).Where("id = ?", c.PostID).
+			Updates(map[string]interface{}{"comments_count": 0, "comment_status": "无评论"})
+		return nil
+	} else {
+		tx.Model(&Post{}).Where("id = ?", c.PostID).Update("comments_count", gorm.Expr("comments_count - 1"))
+		return nil
+	}
+}
+
+func DeleteComment(p *Comment) {
+	db, err := gorm.Open(mysql.Open("root:root@tcp(127.0.0.1:3306)/go_test?charset=utf8mb4&parseTime=True&loc=Local"))
+	if err != nil {
+		panic(err)
+	}
+	if p.PostID <= 0 {
+		db.First(p)
+	}
+	db.Delete(p)
+}
+
+func CreatePost(p *Post) {
+	db, err := gorm.Open(mysql.Open("root:root@tcp(127.0.0.1:3306)/go_test?charset=utf8mb4&parseTime=True&loc=Local"))
+	if err != nil {
+		panic(err)
+	}
+	db.Create(p)
+
+}
--- main.go
+++ main.go
@@ -2,14 +2,21 @@ package main
 
 import (
 	"fmt"
+	"sync"
+	"time"
 
 	"github.com/quaminigin/go_test/class_01"
 	"github.com/quaminigin/go_test/class_02"
+	"github.com/quaminigin/go_test/class_03"
+	"gorm.io/gorm"
 )
 
 func main() {
+	// 第3课习题测试
+	Class_03_Test()
+
 	// 第2课习题测试
-	Class_02_Test()
+	// Class_02_Test()
 
 	// 第1课习题测试
 	// Class_01_Test()
@@ -17,59 +24,94 @@ func main() {
 }
 
 /*
+第3课习题测试
+*/
+func Class_03_Test() {
+	// 1. 基础SQL
+	class_03.BasicSQL("create")
+	class_03.BasicSQL("query")
+	class_03.BasicSQL("update")
+	class_03.BasicSQL("delete")
+
+	// 2. 事务SQL
+	class_03.TransactionSQL()
+
+	// 3. 博客系统建表
+	class_03.BlogCreateTable()
+
+	// 4. 关联查询
+	user, post := class_03.LinkedTableQuery()
+	fmt.Println(user)
+	fmt.Println(post)
+
+	// 5. 钩子函数
+	class_03.CreatePost(&class_03.Post{
+		Title:         "钩子测试",
+		Content:       "钩子测试的内容",
+		CommentsCount: 0,
+		CommentStatus: "无评论",
+		UserID:        3,
+	})
+
+	class_03.DeleteComment(&class_03.Comment{
+		Model: gorm.Model{ID: 4},
+	})
+}
+
+/*
 第2课习题测试
 */
 func Class_02_Test() {
 	// 1. 指针 - 加10
-	// var num int = 100
-	// class_02.Add10(&num)
-	// fmt.Println(num)
+	var num int = 100
+	class_02.Add10(&num)
+	fmt.Println(num)
 
 	// 2. 指针 - 乘以2
-	// numlist := []int{1, 3, 5, 2, 8, 10, 0, 1}
-	// class_02.TimeSliceBy2(&numlist)
-	// fmt.Println(numlist)
+	numlist := []int{1, 3, 5, 2, 8, 10, 0, 1}
+	class_02.TimeSliceBy2(&numlist)
+	fmt.Println(numlist)
 
 	// 3. 协程 - 打印奇偶序列
-	// var wg sync.WaitGroup
-	// wg.Add(2)
-	// class_02.PrintWith2Go(&wg)
-	// wg.Wait()
-	// fmt.Println("Over")
+	var wg sync.WaitGroup
+	wg.Add(2)
+	class_02.PrintWith2Go(&wg)
+	wg.Wait()
+	fmt.Println("Over")
 
 	// 4. 协程 - 任务调度打印时间
-	// class_02.TaskScheduling([]func(){
-	// 	func() {
-	// 		time.Sleep(1 * time.Second)
-	// 	},
-	// 	func() {
-	// 		time.Sleep(4 * time.Second)
-	// 	},
-	// 	func() {
-	// 		time.Sleep(10 * time.Second)
-	// 	},
-	// })
+	class_02.TaskScheduling([]func(){
+		func() {
+			time.Sleep(1 * time.Second)
+		},
+		func() {
+			time.Sleep(4 * time.Second)
+		},
+		func() {
+			time.Sleep(10 * time.Second)
+		},
+	})
 
 	// 5. 面向对象 - 圆和长方形
-	// var s1 class_02.Shape = &class_02.Circle{Radius: 2.0}
-	// var s2 class_02.Shape = &class_02.Rectangle{Width: 3.0, Height: 4.0}
-	// fmt.Println(s1.Area())
-	// fmt.Println(s1.Perimeter())
-	// fmt.Println(s2.Area())
-	// fmt.Println(s2.Perimeter())
+	var s1 class_02.Shape = &class_02.Circle{Radius: 2.0}
+	var s2 class_02.Shape = &class_02.Rectangle{Width: 3.0, Height: 4.0}
+	fmt.Println(s1.Area())
+	fmt.Println(s1.Perimeter())
+	fmt.Println(s2.Area())
+	fmt.Println(s2.Perimeter())
 
 	// 6. 面向对象 - 结构体嵌套
-	// e := class_02.Employee{Person: class_02.Person{Name: "QN", Age: 33}, EmployeeID: "z001"}
-	// e.PrintInfo()
+	e := class_02.Employee{Person: class_02.Person{Name: "QN", Age: 33}, EmployeeID: "z001"}
+	e.PrintInfo()
 
 	// 7. Channel - 传int值
-	// class_02.ChannelOfInt()
+	class_02.ChannelOfInt()
 
 	// 8. Channel - 带有缓冲的通道
-	// class_02.BufferedChannel()
+	class_02.BufferedChannel()
 
 	// 9. 锁机制 - 计数器
-	// class_02.SyncTest()
+	class_02.SyncTest()
 
 	// 10. 锁机制 - 原子操作
 	class_02.AtomicTest()
